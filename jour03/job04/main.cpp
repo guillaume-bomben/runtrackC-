82870ad511e6a955dbb49793eb746472ab46bf9c
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-05-02 07:53:09 +0200

    Add main.cpp file with logic to calculate the length of a string

---
+++ jour03/job04/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string chaine = "Vive la plateforme !";
+    int taille = chaine.length();
+    cout << "La taille est " << taille << endl;
+    return 0;
+}
